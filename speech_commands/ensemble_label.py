a426fb1c908791d43083119bd377c18d057c093b
Feiteng <lifeiteng0422@gmail.com>
2017-12-04 21:35:30 +0800

    label: Add debug config and relabel-logic

--- speech_commands/ensemble_label.py
+++ speech_commands/ensemble_label.py
@@ -27,8 +27,9 @@ from __future__ import print_function
 
 import argparse
 import csv
+import os
+import time
 from operator import itemgetter
-
 
 FLAGS = None
 
@@ -68,6 +69,39 @@ def load_label_csv(csv_file):
     return label
 
 
+class color:
+  d = {}
+  RESET_SEQ = ""
+
+  def __init__(self, c):
+    if c == True:
+      self.d['K'] = "\033[0;30m"  # black
+      self.d['R'] = "\033[0;31m"  # red
+      self.d['G'] = "\033[0;32m"  # green
+      self.d['Y'] = "\033[0;33m"  # yellow
+      self.d['B'] = "\033[0;34m"  # blue
+      self.d['M'] = "\033[0;35m"  # magenta
+      self.d['C'] = "\033[0;36m"  # cyan
+      self.d['W'] = "\033[0;37m"  # white
+      self.RESET_SEQ = "\033[0m"
+    else:
+      self.d['K'] = "["
+      self.d['R'] = "<"
+      self.d['G'] = "["
+      self.d['Y'] = "["
+      self.d['B'] = "["
+      self.d['M'] = "["
+      self.d['C'] = "["
+      self.d['W'] = "["
+      self.RESET_SEQ = "]"
+
+  def c_string(self, color, string):
+    return self.d[color] + string + self.RESET_SEQ
+
+
+colors = color(True)
+
+
 def ensemble_labels(labels, mode):
   final_label = {}
   num_labels = [len(label) for label in labels]
@@ -75,17 +109,42 @@ def ensemble_labels(labels, mode):
     assert any(num_labels[i] == num_labels[0] for i in range(1, len(num_labels)))
 
   labels_list = [label.replace('_', '') for label in load_labels(FLAGS.labels)]
+  label2index = {v: k for k, v in enumerate(labels_list)}
+
+  unknown_index = label2index["unknown"]
+  silence_index = label2index["silence"]
 
   for k in labels[0]:
     scores = [label[k] for label in labels]
     if mode == 'argmax':
       score = [sum(i) for i in zip(*scores)]
-      index, _ = max(enumerate(score), key=itemgetter(1))
+      index, value = max(enumerate(score), key=itemgetter(1))
+
+      def _relabel_to_unknown():
+        if value < 0.9 and index != unknown_index and index != silence_index:
+          if index != unknown_index and score[unknown_index] >= 0.08 and value < 0.9:
+            return True
+        return False
+
+      if FLAGS.debug and value < 0.9 and index != unknown_index and index != silence_index:
+        os.system("play -V0 /Users/feiteng/Geek/Kaggle/TF_Speech/test/audio/{}".format(k))
+        print(
+          "INFO: {} Label: {} score: {}".format(' '.join(["%8s: %.4f\n" % (k, v) for k, v in zip(labels_list, score)]),
+                                                colors.c_string('R', labels_list[index]), value))
+        if _relabel_to_unknown():
+          # final_label[k] = "unknown"
+          print("INFO: re-label from {:8s} -> {:8s}".format(colors.c_string('R', labels_list[index]),
+                                                            colors.c_string('G', 'unknown')))
+        time.sleep(4)
       final_label[k] = labels_list[index]
+      if _relabel_to_unknown():
+        final_label[k] = "unknown"
+
     else:
       raise NotImplementedError
 
   return final_label
+
 
 def main(argv):
   """Entry point for script, converts flags to arguments."""
@@ -106,6 +165,11 @@ if __name__ == '__main__':
     type=str,
     default='argmax',
     help='Ensemble mode.')
+  parser.add_argument(
+    '--debug',
+    action='store_true',
+    default=False,
+    help='Output score instead of label')
 
   FLAGS, unparsed = parser.parse_known_args()
   main(unparsed)
