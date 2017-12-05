a6ece24b675a4290ad5bb114fd8f6f7360dad3fe
Feiteng <lifeiteng0422@gmail.com>
2017-12-05 23:11:21 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -3,13 +3,20 @@
 mode="train"
 resnet_size=20
 model="resnet15"  # resnet
+hparams=""
+batch_size=64
+
 suffix=
 opt=
 
 iter=""
 infer_opt=""
 test_dir="/home/ftli/Data/TF_Speech/test"
-use_gpu=false
+use_gpu=true
+
+
+training_steps="15000,10000"
+learning_rate="0.001,0.0001"
 
 . ./parse_options.sh
 
@@ -18,24 +25,21 @@ set -e
 model_dir=exp/speech_commands_${model}$suffix
 datestr=`date "+DATE: %Y-%m-%d-%H-%M" | awk '{print $2}'`
 
+if $use_gpu;then
+  export CUDA_VISIBLE_DEVICES=0
+else
+  export CUDA_VISIBLE_DEVICES=""
+fi
+
 if [ "$mode" = 'train' ];then
-  export CUDA_VISIBLE_DEVICES=0
-  python speech_commands/train.py --data_dir ~/Data/TF_Speech/speech_commands \
-    --train_dir $model_dir --check_nans False \
-    --how_many_training_steps 15000,15000,15000 --learning_rate 0.001,0.0001,0.00001 \
-    --resnet_size $resnet_size --optimizer "$opt" \
-    --summaries_dir $model_dir/train --model_architecture "$model"
-elif [ "$mode" = 'train2' ];then
-  # DEEP RESIDUAL LEARNING FOR SMALL-FOOTPRINT KEYWORD SPOTTING
-  export CUDA_VISIBLE_DEVICES=0
   python speech_commands/train.py \
     --data_dir ~/Data/TF_Speech/speech_commands \
     --train_dir $model_dir \
-    --how_many_training_steps 10000,10000,10000,10000 --learning_rate 0.001,0.0001,0.00005,0.00001 \
-    --resnet_size $resnet_size --optimizer "$opt" \
+    --how_many_training_steps "$training_steps" --learning_rate "$learning_rate" \
+    --resnet_size $resnet_size --optimizer "$opt" --hparams "$hparams" \
     --summaries_dir $model_dir/summaries \
     --model_architecture "$model" --window_size_ms 30.0 --window_stride_ms 10.0 \
-    --batch_size 64
+    --batch_size $batch_size
 fi
 
 # [ ! -z "$mode" ] && datestr=$mode
@@ -45,11 +49,6 @@ step=`echo $iter | cut -d"-" -f2`
 datestr="step-$step.$datestr"
 echo "datestr = $datestr"
 
-if $use_gpu;then
-  export CUDA_VISIBLE_DEVICES=0
-else
-  export CUDA_VISIBLE_DEVICES=""
-fi
 echo "$0: $model_dir/$iter ======="
 
 mkdir -p submissions
@@ -58,8 +57,10 @@ mkdir -p data
 python speech_commands/infer.py \
   --data_dir $test_dir \
   --train_dir $model_dir \
-  --resnet_size $resnet_size \
+  --resnet_size $resnet_size --hparams "$hparams" \
   --model_architecture "$model" --window_size_ms 30.0 --window_stride_ms 10.0 \
   --batch_size 64 --output_csv $model_dir/output_$datestr.csv </dev/null || exit 1
+
+cp $model_dir/output_$datestr.csv submissions || exit 1
 
 echo "$0: DONE"
