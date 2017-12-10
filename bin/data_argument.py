f76f14da6ba234a1bf6842930216f22d156aff53
Feiteng <lifeiteng0422@gmail.com>
2017-12-10 19:36:04 +0800

    read silence probs and determine trim index

--- bin/data_argument.py
+++ bin/data_argument.py
@@ -71,12 +71,79 @@ def pad_and_write(data, sr, wav_file, desired_samples=16000):
   write_wav_file(data, sr, wav_file)
 
 
-def main(wav_list):
+def get_key(line):
+  wav_file = line.strip()
+  feilds = wav_file.split('/')
+  return feilds[-2] + '_' + feilds[-1].replace('.wav', '')
+
+
+class TrimIndexs(object):
+  def __init__(self, silence_probs_ark, sample_rate=16000, frame_shift=10, window_size=8, threshold=0.4):
+    self.frame_shift = int(sample_rate * frame_shift / 1000.0)
+    self.window_size = window_size
+    self.threshold = threshold
+    self._indexs = {}
+    self._load_probs_ark(silence_probs_ark)
+
+  def _load_probs_ark(self, silence_probs_ark):
+    with open(silence_probs_ark) as f:
+      # sample_02e85b60_nohash_0  [
+      # 0.9961087 0.003891301
+      # 0.996166 0.003834
+      #
+      # 0.6095727 0.3904274 ]
+      chunk = []
+      key = ''
+      for line in f:
+        line = line.strip()
+        if line.find(' ]') > 0:
+          assert key not in self._indexs
+          assert key != ''
+          chunk.append(float(line.split()[0]))
+          self._indexs[key] = self._find_start_end(chunk, key=key)
+          chunk = []
+        elif line.find('  [') > 0:
+          key = line.split()[0]
+        else:
+          chunk.append(float(line.split()[0]))
+
+  def _find_start_end(self, chunk, key=''):
+    start = 0
+    end = len(chunk)
+
+    for i in range(0, len(chunk) - self.window_size):
+      avg = sum(chunk[i:i + self.window_size]) / self.window_size
+      if avg < self.threshold:
+        start = max(i - int(self.window_size / 2), start)
+        break
+    for i in range(len(chunk), self.window_size - 1, -1):
+      avg = sum(chunk[i - self.window_size:i]) / self.window_size
+      if avg < self.threshold:
+        end = min(i + int(self.window_size / 2), end)
+        break
+
+    if end < start + 10:
+      logger.warn(
+        '{} start: {} end: {} probs: {}'.format(key, start, end,
+                                                ' '.join([':'.join([str(k), str(v)]) for k, v in enumerate(chunk)])))
+
+    assert end > start
+    return (start * self.frame_shift, end * self.frame_shift)
+
+  def get_index(self, key):
+    assert key in self._indexs
+    return self._indexs[key]
+
+
+def main(silence_probs_ark, wav_list):
   if not os.path.isfile(wav_list):
     raise ValueError("You must set --data_list <wavfile-list>")
 
   model_settings = prepare_model_settings(FLAGS.sample_rate, FLAGS.clip_duration_ms)
   desired_samples = model_settings['desired_samples']
+
+  trim_indexs = TrimIndexs(silence_probs_ark, FLAGS.sample_rate, frame_shift=10,
+                           window_size=FLAGS.window_size, threshold=FLAGS.threshold)
 
   with open(wav_list) as f:
     for line in f:
@@ -87,14 +154,13 @@ def main(wav_list):
 
       logger.info("Process: {}".format('/'.join(line.split('/')[-2:])))
       data, sr = load_wav_file(line)
-      for db in [25, 30, 35, 40, 50]:
-        data_trim, index = librosa.effects.trim(data / np.max(data), top_db=db, ref=1.0)
-        wav_file = line.replace('.wav', '_trim{}.wav'.format(db))
-        write_wav_file(data[index[0]:index[1]], sr, wav_file)
-      # data_trim, index = librosa.effects.trim(data, top_db=60)
-      # # Random PAD -> store
-      # wav_file = line.replace('.wav', '_trim.wav')
-      # write_wav_file(data_trim, sr, wav_file)
+      # for db in [25, 30, 35, 40, 50]:
+      #   data_trim, index = librosa.effects.trim(data / np.max(data), top_db=db, ref=1.0)
+      #   wav_file = line.replace('.wav', '_trim{}.wav'.format(db))
+      #   write_wav_file(data[index[0]:index[1]], sr, wav_file)
+      index = trim_indexs.get_index(get_key(line))
+      wav_file = line.replace('.wav', '_trim.wav')
+      write_wav_file(data[index[0]:index[1]], sr, wav_file)
 
       # # speed
       # for sp in [1.1, 1.2]:
@@ -114,7 +180,7 @@ def main(wav_list):
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser("Usage: {} wav-list".format(__file__))
+  parser = argparse.ArgumentParser("Usage: {} silence_probs.ark wav-list".format(__file__))
   parser.add_argument(
     '--sample_rate',
     type=int,
@@ -125,9 +191,19 @@ if __name__ == '__main__':
     type=int,
     default=1000,
     help='Expected duration in milliseconds of the wavs', )
+  parser.add_argument(
+    '--window_size',
+    type=int,
+    default=4,
+    help='Trim: silence probs window size', )
+  parser.add_argument(
+    '--threshold',
+    type=float,
+    default=0.5,
+    help='Trim: silence threshold', )
 
   FLAGS, unparsed = parser.parse_known_args()
-  if len(unparsed) != 1:
+  if len(unparsed) != 2:
     parser.print_help()
     exit(1)
-  main(unparsed[0])
+  main(unparsed[0], unparsed[1])
