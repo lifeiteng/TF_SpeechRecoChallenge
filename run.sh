26abe3ae29de7b60a6c41257ed3a4f1990304253
Feiteng <lifeiteng0422@gmail.com>
2018-01-09 22:42:06 +0800

    rm window size shift config

--- run.sh
+++ run.sh
@@ -30,7 +30,6 @@ learning_rate="0.001,0.0001"
 set -e
 
 model_dir=exp/speech_commands_${model}$suffix
-datestr=`date "+DATE: %Y-%m-%d-%H-%M" | awk '{print $2}'`
 
 if $use_gpu;then
   export CUDA_VISIBLE_DEVICES=0
@@ -40,19 +39,20 @@ fi
 
 if [ "$mode" = 'train' ];then
   python speech/train.py \
-    --data_dir $data_dir $train_opts \
+    --data_dir $data_dir \
     --train_dir $model_dir \
     --how_many_training_steps "$training_steps" --learning_rate "$learning_rate" \
-    --resnet_size $resnet_size --optimizer "$opt" --hparams "$hparams" \
+    --optimizer "$opt" --hparams "$hparams" \
     --summaries_dir $model_dir/summaries \
-    --model_architecture "$model" --window_size_ms 30.0 --window_stride_ms 10.0 \
-    --batch_size $batch_size --feature_scaling "$feature_scaling"
+    --model_architecture "$model" \
+    --batch_size $batch_size --feature_scaling "$feature_scaling" $train_opts
   if $skip_infer;then
     echo "$0: skip infer."
     exit 0
   fi
 fi
 
+datestr=`date "+DATE: %Y-%m-%d-%H-%M" | awk '{print $2}'`
 # [ ! -z "$mode" ] && datestr=$mode
 [ -z "$iter" ] && iter=`cat $model_dir/checkpoint | grep "^model_checkpoint_path" | cut -d"\"" -f2`
 
@@ -68,11 +68,11 @@ mkdir -p data
 output_csv=$model_dir/scores${score_prefix}_${model}${suffix}_$datestr.csv
 
 python speech/infer.py \
-  --data_dir $test_dir $train_opts \
-  --train_dir $model_dir \
-  --resnet_size $resnet_size --hparams "$hparams" \
-  --model_architecture "$model" --window_size_ms 30.0 --window_stride_ms 10.0 \
-  --batch_size 64 --output_csv $output_csv --feature_scaling "$feature_scaling" </dev/null || exit 1
+  --data_dir $test_dir \
+  --train_dir $model_dir --hparams "$hparams" \
+  --model_architecture "$model" \
+  --batch_size 48 --output_csv $output_csv \
+  --feature_scaling "$feature_scaling" $train_opts </dev/null || exit 1
 
 cp $output_csv submissions || exit 1
 python speech/ensemble_label.py $output_csv submissions/${model}${suffix}_${score_prefix}$datestr.csv
